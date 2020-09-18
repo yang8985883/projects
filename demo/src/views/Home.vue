8264be012ae4549222b0e1d77b26c70fcc24a41a
yang8985883 <409243668@qq.com>
2020-09-18 22:00:32 +0800

    project init

---
+++ demo/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="home">
+        <van-button type="danger">主要按钮</van-button>
+        <van-list
+            v-model="loading"
+            :finished="finished"
+            @load="onLoad"
+        >
+            <van-cell v-for="item in list" :key="item" :title="item" />
+        </van-list>
+    </div>
+</template>
+
+<script>
+import {List, Button, Cell} from 'vant';
+
+export default {
+    name: 'Home',
+    data(){
+        return {
+            list: [123],
+            loading: false,
+            finished: true,
+        }
+    },
+    components:{
+        [Button.name]: Button,
+        [List.name]: List,
+        [Cell.name]: Cell
+    },
+    methods:{
+        onLoad(){
+
+        }
+    }
+}
+</script>
